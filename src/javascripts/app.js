a6f3de9c585348ad08c1d6b557653d2c216c48d6
Diego Maeoka <diego.maeoka@infomentum.co.uk>
2022-07-18 15:40:47 +0100

    Add elements

--- src/javascripts/app.js
+++ src/javascripts/app.js
@@ -48,8 +48,8 @@ document.addEventListener('DOMContentLoaded', function() {
 	!function() {
 		let t = window.driftt = window.drift = window.driftt || [];
 		if (!t.init) {
-			if (t.invoked) return void(window.console && console.error && console.error("Drift snippet included twice."));
-			t.invoked = !0, t.methods = ["identify", "config", "track", "reset", "debug", "show", "ping", "page", "hide", "off", "on"],
+			if (t.invoked) return void(window.console && console.error && console.error('Drift snippet included twice.'));
+			t.invoked = !0, t.methods = ['identify', 'config', 'track', 'reset', 'debug', 'show', 'ping', 'page', 'hide', 'off', 'on'],
 				t.factory = function(e) {
 					return function() {
 						let n = Array.prototype.slice.call(arguments);
@@ -60,9 +60,9 @@ document.addEventListener('DOMContentLoaded', function() {
 				}), t.load = function(t) {
 					let e = 3e5,
 						n = Math.ceil(new Date() / e) * e,
-						o = document.createElement("script");
-					o.type = "text/javascript", o.async = !0, o.crossorigin = "anonymous", o.src = "https://js.driftt.com/include/" + n + "/" + t + ".js";
-					let i = document.getElementsByTagName("script")[0];
+						o = document.createElement('script');
+					o.type = 'text/javascript', o.async = !0, o.crossorigin = 'anonymous', o.src = 'https://js.driftt.com/include/' + n + '/' + t + '.js';
+					let i = document.getElementsByTagName('script')[0];
 					i.parentNode.insertBefore(o, i);
 				};
 		}
@@ -80,6 +80,35 @@ document.addEventListener('DOMContentLoaded', function() {
 		window.drift.on('chatClose', () => {
 			api.widget.hide()
 		})
-
 	});
+
+	const tl = window.gsap.timeline({
+		defaults: {
+			duration: 1,
+			ease: 'power1.inOut'
+		},
+		scrollTrigger: {
+			trigger: '.illustrations',
+			start: 'center center',
+			end: 'bottom top',
+			scrub: true,
+			pin: true
+		}
+	});
+
+	tl.from('#robot', {
+		opacity: 0,
+		y: innerHeight * 1.125
+	})
+	.from('#tv', {
+		opacity: 0,
+		y: innerHeight * 1.125
+	}, '-=.5')
+
+	// .from('.midsole', {
+	// 	y: innerHeight * 1.5
+	// })
+	// .from('.outsole', {
+	// 	y: innerHeight * 1.5
+	// });
 });
